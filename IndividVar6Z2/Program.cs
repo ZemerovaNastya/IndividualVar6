11f8fc5585b896902a44d44c716ddebcca734a13
ZemerovaNastya <ZemerovaNastyaFS@gmail.com>
2021-10-13 20:57:37 +0500

    Добавьте файлы проекта.

---
+++ IndividVar6Z2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IndividVar6Z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите название файла: ");
+            string path = Console.ReadLine();
+            try
+            {
+                StreamReader sr = new StreamReader(path);
+                Queue<string> q = new Queue<string>();
+                string str;
+                try
+                {
+                    Console.WriteLine("Исходная коллекция");
+                    foreach (string s in File.ReadAllLines(path))
+                    {
+                        q.Enqueue(sr.ReadLine());
+                        Console.WriteLine(s);
+                    }
+                    sr.Close();
+                    if (q.Count == 0)
+                    {
+                        Console.WriteLine("Файл пустой!");
+                    }
+                    else
+                    {
+                        str = q.Peek();
+                        string[] num = str.Split(' ');
+                        q.Clear();
+                        for (int i = 0; i < num.Length; i++)
+                        {
+                            if (int.Parse(num[i]) >= 0)
+                            {
+                                q.Enqueue(num[i] + " ");
+                            }
+                        }
+                        for (int i = 0; i < num.Length; i++)
+                        {
+                            if (int.Parse(num[i]) < 0)
+                            {
+                                q.Enqueue(num[i] + " ");
+                            }
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("Отсортированная коллекция");
+                        foreach (string s in q)
+                        {
+                            Console.Write(s);
+                        }
+                        Console.ReadLine();
+                    }
+                }
+                catch { Console.WriteLine("Содержимое файла имеет неправильный формат!"); }
+            }
+            catch { Console.WriteLine("Имя файла введено неправильно или такого файла нет!"); }
+        }
+    }
+}
---
+++ IndividualVar6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace IndividualVar6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack intStack = new Stack();
+            Console.WriteLine("Введите название файла: ");
+            string path = Console.ReadLine();
+            try
+            {
+                StreamReader sr = new StreamReader(path);
+                Console.WriteLine("Исходная коллекция: ");
+                foreach(char symb in File.ReadAllText(path))
+                {
+                    char c = (char)sr.Read();
+                    intStack.Push(c);
+                    Console.Write(c);
+                }
+                sr.Close();
+                if (intStack.Count == 0)
+                {
+                    Console.WriteLine("Файл пустой");
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Отсортированная коллекция: ");
+                    for (int i = intStack.Count; i > 0; i--)
+                    {
+                        Console.Write(intStack.Pop());
+                    }
+                    Console.ReadLine();
+                }
+            }
+            catch { Console.WriteLine("Имя файла введено неправильно или такого файла нет!"); }
+        }
+    }
+}
